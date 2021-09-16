c72d7c382dd85df8874efe25edb49a053c82114e
kevinM01 <78092182+kevinM01@users.noreply.github.com>
2021-09-16 15:37:13 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello{
+public static void main(String[] args)
+{
+System.out.println("Hello world");
+System.out.println("The numbers are:");
+for(int i=1;i<=5;i++)
+{
+System.out.println("Number="+i);
+}
+}
+}
